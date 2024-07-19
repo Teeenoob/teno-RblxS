d27bf2ed11950eaca0fb762de8b396966887b2d7
Teeenoob <123883603+Teeenoob@users.noreply.github.com>
2024-07-19 16:08:45 +0200

    Create test lyrics file

---
+++ lyrics.lua
@@ -0,0 +1,28 @@
+[00:22.08]To the coffin
+[00:24.68]To the tiles
+[00:27.13]Waiting 'till the afterlife
+[00:32.85]My confession won't suffice
+[00:38.28]There is too much life left in your eyes
+[00:46.87]I know, you know you never betray the truth
+[00:50.38]Too soon
+[00:52.24]The walls they could say
+[00:53.56]" You're just another saint"
+[00:58.24]Try to understand
+[01:03.28]You are just a man
+[01:08.88]The cracks on the pavement, just another sign
+[01:14.36]This roof will collapse to kill us both inside
+[01:28.30]The bride and groom are laid to rest
+[01:30.97]Can I predict what's next?
+[01:33.59]The corridor halls of emptiness
+[01:36.31]Are looking for a guest
+[01:39.15]Sitting in silence I know what we both see
+[01:44.86]I know, I know the end is approaching for you and me
+[01:53.33]Try to understand
+[01:58.80]You are just a man
+[02:04.26]"Promises that won't satisfy"
+[02:09.78]What are you saying?
+[02:13.36]I can't take it anymore
+[02:18.07]You're the one who I adore
+[02:23.57]Wishing I could've been there to save you
+[02:29.13]I know, I know that if I kiss you it might-
+[02:33.17]Just burn us alive
